8663a2663659824fcd235058a90c5d0ffc06419a
jon alden <jondevalden@gmail.com>
2019-09-28 21:44:43 -0500

    started layout for index and questionaire html pages. Added CSS and images for both. Assigned questionaire a path and linked it to the create account anchor in the landing page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
+        msg: "The only place to find fellow car enthusiasts!",
         examples: dbExamples
       });
     });
