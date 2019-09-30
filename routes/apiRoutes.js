47be9c0f6acb679b908deefe94d58fca725b2798
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-30 13:13:59 -0500

    fixed route to get all users

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app, passport) {
   app.get("/api/users", async function(req, res) {
-    const users = await db.Users.findall({});
+    const users = await db.User.findAll({});
     res.json(users);
   });
 
