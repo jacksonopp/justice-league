67a32d4f43a2fb89aaa22954200de97416d4a5b6
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-30 11:29:10 -0500

    added npm package

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,16 +1,20 @@
 // const moment = require("moment");
 
-document.getElementById("signin-btn").addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("click")
-    const body = {
-        username: document.getElementById("username").value,
-        password: document.getElementById("password").value,
-    }
-    console.log({ body });
-    axios.post("/", body).then((response) => {
-        const redirectURL = response.data;
-        console.log(redirectURL);
-        window.location.href = redirectURL;
-    })
-})
+// get the sign in button and add event listener
+document.getElementById("signin-btn").addEventListener("click", e => {
+  // prevent default
+  e.preventDefault();
+  console.log("click");
+  // set up body
+  const body = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value
+  };
+  console.log({ body });
+  // send body in a post request
+  axios.post("/", body).then(response => {
+    const redirectURL = response.data;
+    console.log(redirectURL);
+    window.location.href = redirectURL;
+  });
+});
--- server.js
+++ server.js
@@ -1,31 +1,36 @@
 // imports
-const express = require('express');
-const passport = require('passport');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const exphb = require('express-handlebars');
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const exphb = require("express-handlebars");
 
 // models
 const db = require("./models");
 
 // sync database
-db.sequelize.sync().then(() => {
+db.sequelize
+  .sync()
+  .then(() => {
     console.log("looks good");
-}).catch((err) => {
+  })
+  .catch(err => {
     console.log(err, "something went wrong");
-})
+  });
 
 //set up the app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.static("public"));
 // for bodyparser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // for passport
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })) // session secret
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -38,12 +43,9 @@ require("./routes/apiRoutes")(app, passport);
 require("./routes/htmlRoutes")(app, passport);
 
 //load passport strategies
-require('./config/passport')(passport, db.User);
+require("./config/passport")(passport, db.User);
 
-app.listen(PORT, function (err) {
-
-    if (!err)
-        console.log("Site is live");
-    else console.log(err)
-
+app.listen(PORT, function(err) {
+  if (!err) console.log("Site is live on http://localhost:" + PORT);
+  else console.log(err);
 });
